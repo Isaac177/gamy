68f2a5f711cfae34e5611b8e3d593ae980513fc5
Isaac177 <isaacjogues177@gmail.com>
2024-03-17 17:30:36 +0500

    Initial commit

---
+++ app/src/main/java/com/zeph7/tictactoe/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.zeph7.tictactoe
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.transition.Explode
+import android.view.Window
+import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.requestFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        window.requestFeature(Window.FEATURE_CONTENT_TRANSITIONS)
+        setContentView(R.layout.activity_main)
+
+        val animZoom2 = AnimationUtils.loadAnimation(applicationContext, R.anim.zoom2)
+        val animBlink = AnimationUtils.loadAnimation(applicationContext, R.anim.blink)
+        imgTicTacToe.startAnimation(animBlink)
+
+        btnSingle.setOnClickListener{
+            btnSingle.startAnimation(animZoom2)
+            startActivity(Intent(this@MainActivity, SecondActivity::class.java))
+        }
+
+        btnMulti.setOnClickListener{
+            btnMulti.startAnimation(animZoom2)
+            startActivity(Intent(this@MainActivity, ThirdActivity::class.java))
+        }
+
+        imgTicTacToe.setOnClickListener{
+            imgTicTacToe.startAnimation(animBlink)
+        }
+
+        // back ImageView
+        imageViewBack.setOnClickListener {
+            finish()
+            moveTaskToBack(true)
+        }
+
+        // quit ImageView
+        imageViewQuit.setOnClickListener {
+            finish()
+            moveTaskToBack(true)
+        }
+
+    }
+
+    // for handling back button of the Android Device
+    override fun onBackPressed() {
+        super.onBackPressed()
+        moveTaskToBack(true)
+    }
+
+}
